0588cbf8fb9b9f86b924d5bff2698b2a9da2712b
ryufuta <rfutago@gmail.com>
2023-11-23 17:23:26 +0900

    紛らわしい変数名を変更
    レビューでの指摘事項を反映

--- cafe.rb
+++ cafe.rb
@@ -25,10 +25,10 @@ def take_order(menus)
 end
 
 puts 'bugカフェへようこそ！ご注文は？ 番号でどうぞ'
-order1 = take_order(DRINKS)
+drink_order = take_order(DRINKS)
 
 puts 'フードメニューはいかがですか?'
-order2 = take_order(FOODS)
+food_order = take_order(FOODS)
 
-total = DRINKS[order1][:price] + FOODS[order2][:price]
+total = DRINKS[drink_order][:price] + FOODS[food_order][:price]
 puts "お会計は#{total}円になります。ありがとうございました！"
